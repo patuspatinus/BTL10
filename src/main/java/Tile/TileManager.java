bb34922bab571c7ebf041fdd74bf414bea4f8e22
trung1007 <trungthanhcao.2003@gmail.com>
2022-10-07 13:22:10 +0700

    Update TileManager.java
    trung

--- src/main/java/Tile/TileManager.java
+++ src/main/java/Tile/TileManager.java
@@ -8,32 +8,35 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
-public class TileManager {
+public class TileManager extends Tile{
     GamePanel gp;
-    Tile[] tile;
+    Tile[] tile = new Tile[1000];
     public TileManager(GamePanel gp){
         this.gp=gp;
-        tile=new Tile[10];
+        //tile=new Tile[10];
+        getTileImage();
     }
     public void getTileImage(){
         try{
             tile[0]=new Tile();
-//"res/sprites/grass.png"
-            tile[0].image= ImageIO.read(Objects.requireNonNull(TileManager.class.getResource("res/sprites/grass.png")));
+
+            File grass=new File("res/sprites/grass.png");
+            tile[0].image= ImageIO.read(grass);
 
             tile[1]=new Tile();
-            File f2=new File("res/sprites/brick.png");
-            tile[1].image= ImageIO.read(f2);
+            File brick=new File("res/sprites/brick.png");
+            tile[1].image= ImageIO.read(brick);
 
             tile[2]=new Tile();
-            File f3=new File("res/sprites/wall.png");
-            tile[2].image= ImageIO.read(f3);
+            File wall=new File("res/sprites/wall.png");
+            tile[2].image= ImageIO.read(wall);
 
         }catch (IOException e){
             e.printStackTrace();
         }
     }
     public void draw(Graphics2D g2){
+
         g2.drawImage(tile[0].image,0,0,gp.TileSize,gp.TileSize,null);
     }
 }
